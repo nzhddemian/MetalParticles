a8019a881960bae34986d1900e3d428f18a8e54a
Nezhdanov Demian <48530701+nzhddemian@users.noreply.github.com>
2026-03-24 01:03:51 -0300

    Initial Commit

---
+++ MetalParticles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalParticles
+//
+//  Created by Demian on 24/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalParticles/MetalParticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalParticlesApp.swift
+//  MetalParticles
+//
+//  Created by Demian on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MetalParticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
